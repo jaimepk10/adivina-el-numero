94a78755f753af61ed69f4a26784eeef40c30ec0
Jaime Rubio Lopez <jaime.rubio@innovasur.com>
2024-04-23 21:55:51 +0200

    Creado contador de tiempo en el GameState

--- lib/GameScreen.dart
+++ lib/GameScreen.dart
@@ -126,6 +126,10 @@ class _GameScreenState extends State<GameScreen> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: <Widget>[
+                        Text(
+                          '${gameState.timeRemaining ~/ 60}:${(gameState.timeRemaining % 60).toString().padLeft(2, '0')}',
+                          style: TextStyle(fontSize: 24),
+                        ),
                         Stack(
                           alignment: Alignment.center,
                           children: <Widget>[
--- lib/GameState.dart
+++ lib/GameState.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'dart:math';
 
@@ -7,22 +9,30 @@ class GameState extends ChangeNotifier {
   int _numberToGuess = 0;
   int? _upperBound;
   int? _lowerBound;
+  late Timer _timer;
+  int _timeRemaining = 5 * 60;
 
   GameState({required int min, required int max})
       : _min = min,
         _max = max,
-        _numberToGuess = min + Random().nextInt(max - min);
+        _numberToGuess = min + Random().nextInt(max - min) {
+    startTimer();
+  }
 
   int get min => _min;
   int get max => _max;
   int get numberToGuess => _numberToGuess;
   int? get upperBound => _upperBound;
   int? get lowerBound => _lowerBound;
+  int get timeRemaining => _timeRemaining;
 
   void resetGame() {
     _numberToGuess = _min + Random().nextInt(_max - _min);
     _upperBound = null;
     _lowerBound = null;
+    _timeRemaining = 5 * 60;
+    _timer.cancel();
+    startTimer();
     notifyListeners();
   }
 
@@ -34,5 +44,22 @@ class GameState extends ChangeNotifier {
   void setLowerBound(int value) {
     _lowerBound = value;
     notifyListeners();
+  }
+
+  void startTimer() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_timeRemaining == 0) {
+        timer.cancel();
+      } else {
+        _timeRemaining--;
+        notifyListeners();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
 }
