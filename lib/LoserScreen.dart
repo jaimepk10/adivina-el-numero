16af598bd31966898e6a240d594adfef8d49a882
Jaime Rubio Lopez <jaime.rubio@innovasur.com>
2024-04-24 20:06:27 +0200

    Mejorada pantalla de derrota

--- lib/LoserScreen.dart
+++ lib/LoserScreen.dart
@@ -8,44 +8,45 @@ class LoserScreen extends StatelessWidget {
     final gameState = Provider.of<GameState>(context, listen: false);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Perdiste'),
-        automaticallyImplyLeading: false,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              Icons.sentiment_very_dissatisfied,
-              color: Colors.red,
-              size: 100.0,
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              'Perdiste!',
-              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20.0),
-            Text(
-              'Mejor suerte la próxima vez',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 50.0),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.red,
-              ),
-              onPressed: () {
-                gameState.resetGame();
-                Navigator.pop(context);
-              },
-              child: Text('Volver a jugar'),
-            )
-          ],
+        appBar: AppBar(
+          title: Text('Perdiste'),
+          automaticallyImplyLeading: false,
         ),
-      ),
-    );
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  Icons.sentiment_very_dissatisfied,
+                  color: Colors.red,
+                  size: 100.0,
+                ),
+                const SizedBox(height: 50.0),
+                Text('Perdiste!',
+                    style:
+                        TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 20.0),
+                Text(
+                  'Mejor suerte la próxima vez',
+                  style: TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 50.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.red,
+                  ),
+                  onPressed: () {
+                    gameState.resetGame();
+                    Navigator.pop(context);
+                  },
+                  child: Text('Volver a jugar'),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
--- lib/WinnerScreen.dart
+++ lib/WinnerScreen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'GameScreen.dart';
 import 'GameState.dart';
 
 class WinnerScreen extends StatelessWidget {
