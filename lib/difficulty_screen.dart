1f084774ced18832bdf7b51ee76894ff60d8043e
Jaime Rubio Lopez <jaime.rubio@innovasur.com>
2024-04-28 12:08:08 +0200

    Cambio formato pantalla dificultad a tarjetas con info

--- lib/difficulty_screen.dart
+++ lib/difficulty_screen.dart
@@ -9,46 +9,82 @@ class DifficultyScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Consumer<GameState>(
-      builder: (context, gameState, child) {
-        return SafeArea(
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                const Padding(
-                  padding: EdgeInsets.only(top: 40.0),
-                  child: Text(
-                    'Selecciona la dificultad',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: Consumer<GameState>(
+        builder: (context, gameState, child) {
+          return SafeArea(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.only(top: 40.0),
+                    child: Text(
+                      'Selecciona la dificultad',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: Dificultad.values.map((dificultad) {
-                      return ListTile(
-                        title: Center(child: Text(dificultad.nombre)),
-                        onTap: () {
-                          gameState.setDifficulty(dificultad);
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const GameScreen(),
-                            ),
-                          );
-                        },
-                      );
-                    }).toList(),
+                  Expanded(
+                    child: ListView(
+                      children: Dificultad.values.map((dificultad) {
+                        return Card(
+                          margin: const EdgeInsets.all(10.0),
+                          elevation: 5,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          color: Colors.grey[200],
+                          child: InkWell(
+                              splashColor: Colors.blue.withAlpha(30),
+                              onTap: () {
+                                gameState.setDifficulty(dificultad);
+                                Future.delayed(const Duration(milliseconds: 50),
+                                    () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const GameScreen(),
+                                    ),
+                                  );
+                                });
+                              },
+                              child: ListTile(
+                                contentPadding: EdgeInsets.all(15.0),
+                                leading: Icon(dificultad.icono,
+                                    color: Colors
+                                        .red[700]), // Cambia el color aquí
+                                title: Padding(
+                                  padding: const EdgeInsets.only(bottom: 10.0),
+                                  child: Text(
+                                    dificultad.nombre,
+                                    style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                subtitle: Text(
+                                  'Rango: ${dificultad.min}-${dificultad.max}\n'
+                                  'Tiempo: ${dificultad.tiempo} minutos\n'
+                                  'Intentos: ${dificultad.intentos}',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              )),
+                        );
+                      }).toList(),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
-      },
-    ));
+          );
+        },
+      ),
+    );
   }
 }
