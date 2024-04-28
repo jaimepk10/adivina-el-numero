a0b432eddab2f95d7f9fc5f3844bd8f31625e70c
Jaime Rubio Lopez <jaime.rubio@innovasur.com>
2024-04-28 11:33:22 +0200

    Eliminado el boton de la derecha del number input

--- lib/number_input_form.dart
+++ lib/number_input_form.dart
@@ -23,82 +23,35 @@ class _NumberInputFormState extends State<NumberInputForm> {
     final gameState = Provider.of<GameState>(context);
     final screenWidth = MediaQuery.of(context).size.width;
     final formWidth = screenWidth * 0.5;
-    final buttonPosition = screenWidth * 0.05;
-    const buttonBackgroundColor = Colors.red;
-    const buttonForegroundColor = Colors.white;
 
-    return Stack(
+    return Align(
       alignment: Alignment.center,
-      children: <Widget>[
-        Align(
-          alignment: Alignment.center,
-          child: SizedBox(
-            width: formWidth,
-            child: TextFormField(
-              controller: widget.controller,
-              readOnly: true,
-              onTap: () {
-                FocusScope.of(context).requestFocus(FocusNode());
-              },
-              textAlign: TextAlign.center,
-              decoration: const InputDecoration(
-                labelText: 'Introduce un número',
-                errorStyle: TextStyle(),
-                errorMaxLines: 2,
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Por favor introduce un número';
-                }
-                int num = int.parse(value);
-                if (num > gameState.max) {
-                  return 'El número introducido está fuera del rango permitido';
-                }
-                return null;
-              },
-            ),
+      child: SizedBox(
+        width: formWidth,
+        child: TextFormField(
+          controller: widget.controller,
+          readOnly: true,
+          onTap: () {
+            FocusScope.of(context).requestFocus(FocusNode());
+          },
+          textAlign: TextAlign.center,
+          decoration: const InputDecoration(
+            labelText: 'Introduce un número',
+            errorStyle: TextStyle(),
+            errorMaxLines: 2,
           ),
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Por favor introduce un número';
+            }
+            int num = int.parse(value);
+            if (num > gameState.max) {
+              return 'El número introducido está fuera del rango permitido';
+            }
+            return null;
+          },
         ),
-        Positioned(
-          right: buttonPosition,
-          child: FloatingActionButton(
-            backgroundColor: buttonBackgroundColor,
-            foregroundColor: buttonForegroundColor,
-            onPressed: () {
-              if (widget.formKey.currentState!.validate()) {
-                widget.formKey.currentState!.save();
-                String number = widget.controller.text;
-                setState(() {
-                  int num = int.parse(number);
-                  if (num != gameState.numberToGuess) {
-                    gameState.decreaseTries();
-                  }
-                  if (num == gameState.numberToGuess) {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => WinnerScreen(
-                                guessedNumber: gameState.numberToGuess,
-                              )),
-                    );
-                  } else if (num > gameState.numberToGuess &&
-                      (gameState.upperBound == null ||
-                          num < gameState.upperBound!)) {
-                    gameState.setUpperBound(num);
-                  } else if (num < gameState.numberToGuess &&
-                      (gameState.lowerBound == null ||
-                          num > gameState.lowerBound!)) {
-                    gameState.setLowerBound(num);
-                  }
-                  widget.controller.clear();
-                });
-              }
-            },
-            shape: const CircleBorder(),
-            child: const Icon(Icons.check),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
